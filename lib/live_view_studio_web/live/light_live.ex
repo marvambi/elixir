56391d5051be3c9d3754543cf4c79bb2e3926b85
Marvin Ambrose <marvambi@primal.com.ng>
2020-08-13 14:07:05 +0100

    Added the Brighten and Dim buttons and logic

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -2,7 +2,7 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :brightness, 5)
+    socket = assign(socket, :brightness, 10)
     {:ok, socket}
   end
 
